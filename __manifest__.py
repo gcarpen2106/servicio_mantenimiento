1976c5a3b3c0d3ef8e651acb345ca9cf5c0ca205
gcarpen2106 <gcarpen2106@gmail.com>
2025-02-10 16:12:39 +0100

    Editado archivo __manifesst__.py

---
+++ __manifest__.py
@@ -0,0 +1,17 @@
+# servicio_mantenimiento/__manifest__.py
+
+{
+    'name': 'Servicio de Mantenimiento',
+    'version': '1.0',
+    'author': 'Gonzalo Carretero',
+    'category': 'Custom',
+    'summary': 'Gestión de Mantenimiento de Productos',
+    'depends': ['base', 'garantia.producto'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/servicio_mantenimiento_views.xml',
+    ],
+    'icon': '/garantias/static/description/icon55.png',
+    'installable': True,
+    'application': True,
+}
