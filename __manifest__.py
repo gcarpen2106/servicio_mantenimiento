13fde30d5e826fd1061e7b9a38d5e550b3b2989b
gcarpen2106 <gcarpen2106@gmail.com>
2025-02-11 08:53:51 +0100

    Arreglando archivo manifest y csv de security

--- __manifest__.py
+++ __manifest__.py
@@ -6,12 +6,12 @@
     'author': 'Gonzalo Carretero',
     'category': 'Custom',
     'summary': 'Gestión de Mantenimiento de Productos',
-    'depends': ['base', 'garantia.producto'],
+    'depends': ['base', 'garantias'],
     'data': [
         'security/ir.model.access.csv',
         'views/servicio_mantenimiento_views.xml',
     ],
-    'icon': '/garantias/static/description/icon55.png',
+    'icon': '/servicio_mantenimiento/static/description/icon55.png',
     'installable': True,
     'application': True,
 }
